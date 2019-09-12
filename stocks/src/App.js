0e067a6ed7715d6e69aa7032222c99087ee6afb3
Laura-Ella <lauraella.robinson@gmail.com>
2019-09-12 10:37:51 -0400

    watchlist component

--- stocks/src/App.js
+++ stocks/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import {Link, Route} from 'react-router-dom'
+import Stocks from './components/Stocks/Stocks'
+import Watchlist from './components/Watchlist/Watchlist'
 import axios from 'axios';
 import "./App.css";
 
@@ -14,14 +17,24 @@ class App extends Component {
     axios
     .get('https://stocks-api-lr.herokuapp.com/')
     .then(res => {
+      this.setState({stocks: res.data})
       console.log(res)
+      console.log(this.state.stocks)
+    })
+    .catch(err => {
+      console.error(err)
     })
   }
 
   render() {
+    console.log(this.state)
     return (
       <div>
-        <p>Text</p>
+        <Link to="/stocks">Stocks</Link>
+        <Link to="/watchlist">Watchlist</Link>
+        <Route path="/stocks" exact render={routerProps => (
+        <Stocks stocks={this.state.stocks} {...routerProps} />)} />
+        <Route path="/watchlist" exact component={Watchlist} />
       </div>
     );
   }
--- stocks/src/components/Stocks/Stocks.js
+++ stocks/src/components/Stocks/Stocks.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 
 class Stocks extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stocks: []
-    };
-  }
-
   render() {
-    return <div>Test</div>;
+    console.log(this.props.stocks);
+    return (
+      <div>
+        <h1>Stocks</h1>
+        <ul>
+          {this.props.stocks.map(stock => (
+            <li>{stock.symbol} - ${stock.bid} - ${stock.ask} - {stock.volume} shares</li>
+          ))}
+          <button>Add to Watchlist</button>
+        </ul>
+      </div>
+    );
   }
 }
 
